83ab439cc6c1480cd0cca0a34713194a040e2272
Andrew Johnston <asjohnston@alaska.edu>
2018-03-13 17:10:51 +0000

    use product name for batch job name, add logging for lambda

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 import boto3
-from uuid import uuid4
-from os import environ
+from urlparse import urlparse
+from os import environ, path
 from logging import getLogger
 
 
 log = getLogger()
-
+log.setLevel(environ['LOG_LEVEL'])
 
 def lambda_handler(event, context):
     batch = boto3.client('batch')
     for url in event['urls']:
+        url_path = urlparse(url).path
+        file_name = path.split(url_path)[-1]
+        product_name = path.splitext(file_name)[0]
+        log.info('Submitting job for %s', product_name)
         batch.submit_job(
-            jobName=str(uuid4()),
+            jobName=product_name,
             jobQueue=environ['JOB_QUEUE'],
             jobDefinition=environ['JOB_DEFINITION'],
             parameters={'url': url},
